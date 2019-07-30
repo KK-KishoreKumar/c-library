b8b249b8f1d9723799a8ba1d4adaa0a0ab03a59f
taisoncar <carsontai0714@gmail.com>
2019-07-30 11:13:14 +0800

    n

---
+++ b.c
@@ -0,0 +1,21 @@
+#include"clibrary.h"
+#include <stdio.h>
+
+int main()
+{
+    int arr[100];
+    int size;
+
+    printf("size:");
+    scanf("%d",&size);
+    printf("input:\n");
+
+    for(int i=0;i<size;++i)
+        scanf("%d",&arr[i]);
+        
+    csort(arr,size);
+    printf("output:\n");
+    for (int p=0;p<size;++p)
+        printf("%d\n",arr[p]);
+}
+
--- c.c
+++ c.c
@@ -69,11 +69,14 @@ int main()
 {
     int arr[100];
     int size;
+
     printf("size:");
     scanf("%d",&size);
     printf("input:\n");
+
     for(int i=0;i<size;++i)
         scanf("%d",&arr[i]);
+        
     csort(arr,size);
     printf("output:\n");
     for (int p=0;p<size;++p)
